d5288b9019439362843c1e7c4c7d385120c47889
Tucker Kern <tuckkern@gmail.com>
2020-03-11 19:33:02 -0600

    Turn system power on via a Kasa smartstrip
    Adjust logging to be cleaner in syslog

--- gmrender-resurrect-monitor.py
+++ gmrender-resurrect-monitor.py
@@ -3,10 +3,14 @@
 import threading
 import argparse
 import logging
+import time
 
 # DBus interaction
 import pydbus
 from gi.repository import GLib
+
+# Smart switch control
+import pyHS100 as kasa
 
 def state_signal_callback(sender, object, iface, signal, params):
   state, *remaining = params
@@ -30,7 +34,7 @@ class SystemMonitor():
         self.Activate()
 
       if self.timer and self.timer.is_alive():
-        logger.info("Disabled shutdown timer.")
+        logger.debug("Disabled shutdown timer.")
         self.timer.cancel()
 
       # Delete existing timer
@@ -42,7 +46,7 @@ class SystemMonitor():
       assert(self.timer == None)
 
       # Start shutdown timer with long interval
-      logger.info("Starting long ({0} s) shutdown timer".format(self.long_timeout))
+      logger.debug("Starting long ({0} s) shutdown timer".format(self.long_timeout))
       self.timer = threading.Timer(self.long_timeout, self.Deactivate)
       self.timer.start()
       self.paused = True
@@ -61,16 +65,38 @@ class SystemMonitor():
         return
 
       # Start shutdown timer with short interval
-      logger.info("Starting short ({0} s) shutdown timer".format(self.short_timeout))
+      logger.debug("Starting short ({0} s) shutdown timer".format(self.short_timeout))
       self.timer = threading.Timer(self.short_timeout, self.Deactivate)
       self.timer.start()
 
   def Activate(self):
-    print("ENABLING SYSTEM POWER")
+    logger.info("Enabling System Power")
+
+    # Preamp
+    strip.turn_on(index=0)
+    time.sleep(1)
+    # Amp 1
+    strip.turn_on(index=1)
+    time.sleep(1)
+    # Amp 2
+    strip.turn_on(index=2)
+    time.sleep(1)
+
     self.active = True
 
   def Deactivate(self):
-    print("DISABLING SYSTEM POWER")
+    logger.info("Disabling System Power")
+
+    # Amp 2
+    strip.turn_off(index=2)
+    time.sleep(1)
+    # Amp 1
+    strip.turn_off(index=1)
+    time.sleep(1)
+    # Preamp
+    strip.turn_off(index=0)
+    time.sleep(1)
+
     self.active = False
     
     # Stop and destroy timer if present
@@ -79,16 +105,44 @@ class SystemMonitor():
       self.timer = None
 
 if __name__ == "__main__":
+  # Basic log config
+  logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+  logger = logging.getLogger("gmrender-resurrect-monitor")
+
   # Argument parsing
   parser = argparse.ArgumentParser(description="Automate system power by subscribing to gmrender-resurrect D-Bus signals.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument("--pause-timeout", help="Disable system power when paused for this duration (seconds)", default=60, type=int)
-  parser.add_argument("--stop-timeout", help="Disable system power when stopped for this duration (seconds)", default=5, type=int)
+  parser.add_argument("--pause_timeout", help="Disable system power when paused for this duration (seconds)", default=60, type=int)
+  parser.add_argument("--stop_timeout", help="Disable system power when stopped for this duration (seconds)", default=5, type=int)
   parser.add_argument("--uuid", help="Monitor for a specific gmedia-resurrect instance given by the UUID")
+  parser.add_argument("--discover", help="List Kasa devices discovered on the network", action="store_true")
+  parser.add_argument("kasa_device_alias", help="Kasa device to control", nargs="?", default=None)
   args = parser.parse_args()
+  
+  if args.discover == False and args.kasa_device_alias is None:
+    logger.error("Target Kasa device alias must be supplied.")
+    exit()
 
-  # Basic log config
-  logging.basicConfig(level=logging.INFO)
-  logger = logging.getLogger("gmrender-resurrect-monitor")
+  # Discover available devices
+  logger.info("Discovering Kasa devices.")
+  kasa_devices = kasa.Discover.discover(timeout=1).values()
+  logger.info("Found {0} Kasa devices.".format(len(kasa_devices)))
+
+  # Dump discovered devices if requested
+  if args.discover:
+    logger.info("Discovered Kasa devices:")
+    for device in kasa_devices:
+      logger.info(device)
+    exit()
+  
+  # Find first device with matching alias
+  strip = next((x for x in kasa_devices if x.alias == args.kasa_device_alias), None)
+
+  if strip is None:
+    logger.error("Could not find Kasa device '{0}'".format(args.kasa_device_alias))
+    exit()
+
+  #strip = kasa.SmartStrip("192.168.1.101")
+  logger.info("Using Kasa device '{0}'".format(strip.alias))
 
   # Create system monitor object to handle state
   monitor = SystemMonitor(args.pause_timeout, args.stop_timeout)
